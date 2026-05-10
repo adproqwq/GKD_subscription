431728059480ead4d0ced52f1014be4bc7037f98
cjy0812 <cjy081219@Gmail.com>
2026-05-10 15:34:34 +0800

    feat: 腾讯体育 全屏广告-首页 (#154)
    close: #154

--- src/apps/com.tencent.qqsports.ts
+++ src/apps/com.tencent.qqsports.ts
@@ -20,5 +20,23 @@ export default defineGkdApp({
         },
       ],
     },
+    {
+      key: 1,
+      name: '全屏广告-首页',
+      desc: '进入app-首页后出现,限时18s',
+      matchTime: 18000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      fastQuery: true,
+      rules: [
+        {
+          activityIds: '.ui.SplashActivity',
+          matches:
+            '@ImageView[childCount=0] - * > [vid="webViewRL"] -> [text="图片"] <<4 [vid="webViewRL"]',
+          snapshotUrls: 'https://i.gkd.li/i/27564093',
+          exampleUrls: 'https://e.gkd.li/42b7abd7-6480-44af-897c-b397f6c01fd2',
+        },
+      ],
+    },
   ],
 });
