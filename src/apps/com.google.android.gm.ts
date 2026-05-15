a1868daa208f7c5a735aa44b467247b769c023f5
Ling0402 <cjy081219@Gmail.com>
2026-05-15 18:34:48 +0800

    fix: Gmail 分段广告-信息流广告(#119) (pr #129)
    close: #119

--- src/apps/com.google.android.gm.ts
+++ src/apps/com.google.android.gm.ts
@@ -7,50 +7,28 @@ export default defineGkdApp({
     {
       key: 2,
       name: '分段广告-信息流广告',
+      desc: '右滑删除⚠️该规则需v1.12.0及以上版本可使用',
       fastQuery: true,
       rules: [
         {
           key: 1,
-          actionMaximum: 1, // https://github.com/Lin-arm/GKD_subscription/issues/119
           matches:
-            '[vid="basic_ad_teaser_info_icon" || vid="button_chip_ad_teaser_info_icon"][clickable=true]',
+            '@[childCount=7][vid$="ad_teaser_item"] >2 [vid="ad_badge_text"]',
+          action: 'swipe',
+          swipeArg: {
+            start: {
+              left: 'width * 0.15',
+              top: 'width * 0.08',
+            },
+            end: {
+              left: 'width * 0.64',
+              top: 'width * 0.08',
+            },
+            duration: 114,
+          },
           snapshotUrls: [
             'https://i.gkd.li/i/13255698', // 旧快照 节点还未引进 vid 属性
             'https://i.gkd.li/i/25542293',
-          ],
-        },
-        {
-          preKeys: [1],
-          key: 2,
-          name: '②点击[屏蔽]',
-          action: 'clickCenter', // 此界面不接受无障碍事件
-          matches:
-            '@Button[clickable=true][text="屏蔽此广告" || text^="Block" || desc="屏蔽"] <<n [vid="my_ad_center_dialog"]',
-          snapshotUrls: [
-            'https://i.gkd.li/i/13255700', // 屏蔽此广告
-            'https://i.gkd.li/i/13724271', // Block
-            'https://i.gkd.li/i/20585351', // 屏蔽
-          ],
-        },
-        {
-          preKeys: [2],
-          key: 3,
-          name: '③点击[继续]',
-          matches:
-            '@Button[clickable=true][text="继续" || text="Continue"] <<n [vid="my_ad_center_dialog"]',
-          snapshotUrls: [
-            'https://i.gkd.li/i/13255701', // 继续
-            'https://i.gkd.li/i/13724287', // Continue
-          ],
-        },
-        {
-          preKeys: [3],
-          key: 4,
-          name: '④x掉',
-          matches: '[vid="my_ad_center_close_icon"][clickable=true]',
-          snapshotUrls: [
-            'https://i.gkd.li/i/13263279',
-            'https://i.gkd.li/i/25543406',
           ],
         },
       ],
