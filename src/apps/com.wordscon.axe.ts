ccc77eec04e26d16e6da586d7350afaea15e99b4
Lin-arm <85251920+Lin-arm@users.noreply.github.com>
2026-05-25 23:47:33 +0800

    perf: 句子控 分段广告(合并)

--- src/apps/com.wordscon.axe.ts
+++ src/apps/com.wordscon.axe.ts
@@ -34,18 +34,19 @@ export default defineGkdApp({
     },
     {
       key: 3,
-      name: '分段广告-复制成功',
-      desc: '长按句子复制成功后出现的弹窗',
+      name: '分段广告',
+      desc: '①点击x掉 ②点击[不感兴趣] ③点击[我知道了]',
       fastQuery: true,
       activityIds: [
         '.sentence.AXPostDetailActivity',
         '.main.MainActivity',
         '.search.AXSquareSearchActivity',
+        '.topic.AXDetailTopicActivity', //热门话题页
       ],
       rules: [
         {
-          key: 1,
-          name: '①点击x掉',
+          key: 0,
+          name: '①点击[x]掉',
           matches:
             '@[clickable=true] < [desc^="dislike"] -2 [childCount=2] >2 [text="广告"]',
           snapshotUrls: [
@@ -56,40 +57,40 @@ export default defineGkdApp({
           exampleUrls: 'https://e.gkd.li/6a509f3c-811f-49b0-8314-8aaf7b604e5c',
         },
         {
-          key: 2,
-          preKeys: [1],
-          name: '②点击[不感兴趣]',
-          matches: '@[clickable=true] >2 [text="不感兴趣"]',
-          snapshotUrls: 'https://i.gkd.li/i/28225930',
-          exampleUrls: 'https://e.gkd.li/7847c022-0eec-4d9a-8649-4ed6bde51f29',
-        },
-      ],
-    },
-    {
-      key: 4,
-      name: '分段广告-信息流广告',
-      desc: '①点击x掉 ②点击[不感兴趣] ③点击[我知道了]',
-      fastQuery: true,
-      activityIds: '.topic.AXDetailTopicActivity', //热门话题页
-      rules: [
-        {
           key: 1,
           name: '①点击x掉',
           matches:
             '@Image[width<69 && height<69][bottom<getPrev(3).top] < View[index=1] <<n [vid="layout_ad_container"] <<n [vid="layout_detail"] + [vid="bt_response"]',
-          snapshotUrls: 'https://i.gkd.li/i/28226759', // 防误右下角触悬浮按键: [bottom<getPrev(3).top]
+          snapshotUrls: 'https://i.gkd.li/i/28226759', // 防误触右下角悬浮按键: [bottom<getPrev(3).top]
           exampleUrls: 'https://e.gkd.li/dae5c8a2-77b4-428d-8568-e9a04bd773e9',
         },
         {
           key: 2,
-          preKeys: [1],
+          name: '①x掉',
+          matches:
+            '@ImageView[width<71 && height<71] <4 [childCount=4] -2 FrameLayout >3 [text^="跳转至"]',
+          snapshotUrls: 'https://i.gkd.li/i/28227993',
+          exampleUrls: 'https://e.gkd.li/6ed41a6d-5e89-4e86-b1d9-de977ca128c0',
+        },
+
+        // 第二段
+        {
+          key: 20,
+          preKeys: [0, 1, 2],
           name: '②点击[不感兴趣]',
           matches: '@[clickable=true] >2 [text="不感兴趣"]',
-          snapshotUrls: 'https://i.gkd.li/i/28226757',
+          snapshotUrls: [
+            'https://i.gkd.li/i/28225930',
+            'https://i.gkd.li/i/28226757',
+            'https://i.gkd.li/i/28228086',
+          ],
+          exampleUrls: 'https://e.gkd.li/7847c022-0eec-4d9a-8649-4ed6bde51f29',
         },
+
+        // 第三段
         {
-          key: 3,
-          preKeys: [2],
+          key: 30,
+          preKeys: [20],
           name: '③点击[我知道了]',
           matches: '[text="开通会员"] -2 [vid="tv_cancel"]',
           snapshotUrls: 'https://i.gkd.li/i/28226756',
