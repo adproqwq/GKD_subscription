69c674059856326c37987e1b3a2165fb66daaab3
cjy0812 <cjy081219@Gmail.com>
2026-05-27 15:57:21 +0800

    fix: 汽水音乐 功能类-全自动看广告领VIP
    fix: key:0, preKeys: [3, 6]

--- src/apps/com.luna.music.ts
+++ src/apps/com.luna.music.ts
@@ -117,17 +117,26 @@ export default defineGkdApp({
       rules: [
         // 主页界面
         {
-          name: '开屏自动看视频', // 启动App出现的弹窗
+          // 主页界面
+          name: '开屏自动看视频',
           key: 0,
           fastQuery: true,
           activityIds: 'com.luna.biz.main.main.MainActivity',
           actionMaximum: 1,
           priorityTime: 5000,
           resetMatch: 'app',
-          matches:
+          anyMatches: [
             '[vid="ui"] >3 @[clickable=true] < ViewGroup +3 View > [text="今日畅听"] + [text^="第" || text$="个"]',
-          snapshotUrls: 'https://i.gkd.li/i/26758188',
-          exampleUrls: 'https://e.gkd.li/194773d6-a9c0-48c4-84bf-e1a57449434b',
+            '@ViewGroup[childCount=0] < ViewGroup[index=2] <n [childCount=5] <<6 FrameLayout <<4 [id="android:id/content"]',
+          ],
+          snapshotUrls: [
+            'https://i.gkd.li/i/26758188',
+            'https://i.gkd.li/i/28299711', // 新UI_2026.05.27
+          ],
+          exampleUrls: [
+            'https://e.gkd.li/194773d6-a9c0-48c4-84bf-e1a57449434b',
+            'https://e.gkd.li/9cd0c931-5ae8-4739-8a1a-481d2d5731f4',
+          ],
         },
         // 视频播放界面
         // 旧版节点树(轮询法)
@@ -378,10 +387,18 @@ export default defineGkdApp({
           actionMaximum: 1,
           priorityTime: 5000,
           resetMatch: 'app',
-          matches:
+          anyMatches: [
             '[vid="ui"] >3 @[clickable=true] < ViewGroup +3 View > [text="今日畅听"] + [text^="第" || text$="个"]',
-          snapshotUrls: 'https://i.gkd.li/i/26758188',
-          exampleUrls: 'https://e.gkd.li/194773d6-a9c0-48c4-84bf-e1a57449434b',
+            '@ViewGroup[childCount=0] < ViewGroup[index=2] <n [childCount=5] <<6 FrameLayout <<4 [id="android:id/content"]',
+          ],
+          snapshotUrls: [
+            'https://i.gkd.li/i/26758188',
+            'https://i.gkd.li/i/28299711', // 新UI_2026.05.27
+          ],
+          exampleUrls: [
+            'https://e.gkd.li/194773d6-a9c0-48c4-84bf-e1a57449434b',
+            'https://e.gkd.li/9cd0c931-5ae8-4739-8a1a-481d2d5731f4',
+          ],
         },
         // 视频播放界面
         // 旧版节点树(轮询法)
@@ -537,16 +554,17 @@ export default defineGkdApp({
           name: '④再看视频?-返回操作',
           fastQuery: true,
           activityIds: 'com.luna.biz.main.main.MainActivity',
-          matches: [
-            '[text$="畅听" || desc$="畅听"]',
+          anyMatches: [
             '[text^="再看" || desc^="再看"][text$="个提前领" || desc$="个提前领"]',
+            '@ViewGroup[childCount=0] <<2 [childCount=4] <<6 FrameLayout <<4 [id="android:id/content"]',
           ],
           action: 'back',
           snapshotUrls: [
             'https://i.gkd.li/i/26411131',
             'https://i.gkd.li/i/26905455',
+            'https://i.gkd.li/i/28299305', // 新UI_2026.05.27
           ],
-          exampleUrls: 'https://e.gkd.li/d3902ed0-5e8d-4c0c-b8ae-5bf3f64c84a8',
+          exampleUrls: 'https://e.gkd.li/6fedf579-d3bc-46eb-b29f-679f469dcfea', //'https://e.gkd.li/d3902ed0-5e8d-4c0c-b8ae-5bf3f64c84a8',
         },
         //其他情况-无视频
         {
