807ee94a2a2860c67d600a197ff1cab5b71d7fa7
Lin-arm <85251920+Lin-arm@users.noreply.github.com>
2026-05-31 16:22:06 +0800

    feat: 医考帮 局部广告...等
    - 调整 全屏广告
    - 新增 局部广告
    - 新增 局部广告-悬浮窗广告

--- src/apps/com.yikaobang.yixue.ts
+++ src/apps/com.yikaobang.yixue.ts
@@ -6,16 +6,43 @@ export default defineGkdApp({
   groups: [
     {
       key: 1,
-      name: '全屏广告-会员活动弹窗',
-      fastQuery: true,
-      matchTime: 10000,
-      actionMaximum: 1,
-      resetMatch: 'app',
+      name: '全屏广告-弹窗广告',
+      desc: '点击x掉',
       rules: [
         {
+          fastQuery: true,
           activityIds: 'com.psychiatrygarden.activity.HomePageNewActivity',
-          matches: '@ImageView[vid="close"]',
-          snapshotUrls: 'https://i.gkd.li/i/13425853',
+          matches: 'ImageView[vid="close"]',
+          snapshotUrls: [
+            'https://i.gkd.li/i/13425853', // 无vid旧快照
+            'https://i.gkd.li/i/28434183',
+          ],
+        },
+      ],
+    },
+    {
+      key: 2,
+      name: '局部广告',
+      desc: '顶部轮播图广告',
+      rules: [
+        {
+          fastQuery: true,
+          activityIds: 'com.psychiatrygarden.activity.HomePageNewActivity',
+          matches: '[vid="iv_ad_close"]',
+          snapshotUrls: 'https://i.gkd.li/i/28434131',
+        },
+      ],
+    },
+    {
+      key: 3,
+      name: '局部广告-悬浮窗广告',
+      desc: '点击x掉',
+      rules: [
+        {
+          fastQuery: true,
+          activityIds: 'com.psychiatrygarden.activity.HomePageNewActivity',
+          matches: 'ImageSwitcher - [vid="iv_delete"]',
+          snapshotUrls: 'https://i.gkd.li/i/28434131',
         },
       ],
     },
