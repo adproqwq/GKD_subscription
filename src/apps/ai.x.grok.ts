24f35dccab47b639d16ecf6741ff0117ef7509e0
Ling0402 <cjy081219@Gmail.com>
2026-06-03 23:33:51 +0800

    feat: Grok 功能类-发生错误自动重试

--- src/apps/ai.x.grok.ts
+++ src/apps/ai.x.grok.ts
@@ -21,5 +21,21 @@ export default defineGkdApp({
         },
       ],
     },
+    {
+      key: 2,
+      name: '功能类-发生错误自动重试',
+      desc: '发送提示词,发生错误=> 重试(多为网络错误)',
+      actionCd: 500, // 500ms 模拟手速快速重试,试图顶除代理出现SSL错误/IPV6
+      rules: [
+        {
+          fastQuery: true,
+          activityIds: '.main.GrokActivity',
+          matches:
+            'Button - TextView < @[childCount=2][clickable=true] <2 View[childCount=2] < [index=parent.childCount.minus(1)] <n * <<(12-n) [id="android:id/content"]',
+          snapshotUrls: 'https://i.gkd.li/i/28557954',
+          exampleUrls: 'https://e.gkd.li/b652da53-192a-4bd8-b214-e55b0f40172d',
+        },
+      ],
+    },
   ],
 });
