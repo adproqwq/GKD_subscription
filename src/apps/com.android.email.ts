20ac1e17d55a2980b60e195327b2e4490bd8479d
Lin-arm <85251920+Lin-arm@users.noreply.github.com>
2026-06-07 17:10:12 +0800

    fix: 电子邮件 功能类-自动切换到[合并账户]

--- src/apps/com.android.email.ts
+++ src/apps/com.android.email.ts
@@ -2,13 +2,13 @@ import { defineGkdApp } from '@gkd-kit/define';
 
 export default defineGkdApp({
   id: 'com.android.email',
-  name: '电子邮件',
+  name: '(小米)电子邮件',
   groups: [
     {
       // 小米自带电子邮件
       key: 1,
-      name: '权限提示-【小米】自启动',
-      desc: '点击取消',
+      name: '权限提示-自启动',
+      desc: '点击[取消]',
       matchTime: 10000,
       actionMaximum: 1,
       resetMatch: 'app',
@@ -17,7 +17,7 @@ export default defineGkdApp({
           fastQuery: true,
           activityIds: 'com.wps.multiwindow.main.HomeActivity',
           matches: [
-            '[text^="检测到您的自启动权限未开启"]',
+            '[text*="自启动权限未开启"]',
             '[text="取消"][clickable=true]',
           ],
           exampleUrls: 'https://e.gkd.li/1eacdaf8-1405-44d8-a9b9-228e913c5180',
@@ -27,28 +27,30 @@ export default defineGkdApp({
     },
     {
       key: 2,
-      name: '功能类-【小米】非合并账户自动切换到合并账户',
+      name: '功能类-自动切换到[合并账户]',
       desc: '仅在收件箱界面开屏时触发',
+      fastQuery: true,
+      activityIds: 'com.wps.multiwindow.main.HomeActivity',
       rules: [
         {
           key: 0,
-          fastQuery: true,
           matchTime: 5000,
           actionMaximum: 1,
           resetMatch: 'app',
-          activityIds: 'com.wps.multiwindow.main.HomeActivity',
           matches:
-            '@[vid="titleContainer"][clickable=true] > [text!="合并账户"] - [text="收件箱"]',
+            '[!(text="合并账户" || text="合并帐户")] <2 [vid="titleContainer"][clickable=true]',
           snapshotUrls: 'https://i.gkd.li/i/25607057',
+          excludeSnapshotUrls: 'https://i.gkd.li/i/28678424',
         },
         {
           key: 1,
           preKeys: 0,
-          fastQuery: true,
           actionDelay: 200, // 目标节点不是 clickable 的，等动画放完再点
-          activityIds: 'com.wps.multiwindow.main.HomeActivity',
-          matches: '[text="合并账户"][checkable=true]',
-          snapshotUrls: 'https://i.gkd.li/i/25607113',
+          matches: '[text="合并账户" || text="合并帐户"][checkable=true]',
+          snapshotUrls: [
+            'https://i.gkd.li/i/25607113',
+            'https://i.gkd.li/i/28678374',
+          ],
         },
       ],
     },
