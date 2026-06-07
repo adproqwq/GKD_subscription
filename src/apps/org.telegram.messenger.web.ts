5a91c2c613bbecdf600e3788560f77ac7075ca20
Lin-arm <85251920+Lin-arm@users.noreply.github.com>
2026-06-07 20:28:29 +0800

    feat: Telegram 功能类规则+2
    - 从隔壁 Google Play版 Telegram 搬来的,暂无快照
    - 功能类-更多-保存到相册
    - 功能类-只保存单个媒体
    
    (两个版本的Telegram只是包名不一样,那些页面节点基本一样,快照等后面有人遇到失效或误触了再补上)

--- src/apps/org.telegram.messenger.web.ts
+++ src/apps/org.telegram.messenger.web.ts
@@ -52,5 +52,47 @@ export default defineGkdApp({
         },
       ],
     },
+    {
+      key: 4,
+      name: '功能类-更多-保存到相册',
+      desc: '右上角菜单-点击[保存到相册]',
+      rules: [
+        {
+          fastQuery: true,
+          activityIds: 'org.telegram.ui.LaunchActivity',
+          matches:
+            '@[clickable=true][left>350] > [text="保存到相册" || text="儲存到相簿" || text="Save to Gallery"]',
+          // 暂无快照, 参考 Google Play版 Telegram ,包名: org.telegram.messenger
+          // snapshotUrls: [
+          //   'https://i.gkd.li/i/26645432', // 保存到相册
+          //   'https://i.gkd.li/i/27050604', // 儲存到相簿
+          //   'https://i.gkd.li/i/26645464', // Save to Gallery
+          // ],
+          // excludeSnapshotUrls: 'https://i.gkd.li/i/26645468', // [left=83]
+          exampleUrls: 'https://e.gkd.li/f164341a-32fa-4054-9ae1-85d5e686bd64',
+        },
+      ],
+    },
+    {
+      key: 5,
+      name: '功能类-只保存单个媒体',
+      desc: '弹窗-点击 [这张图片]或[该媒体]',
+      rules: [
+        {
+          fastQuery: true,
+          matches:
+            '[text^="保存" || text^="儲存" || text^="Save"] < * +2 * > [text^="这" || text^="這" || text^="This" || text^="该"][clickable=true]',
+          // 暂无快照, 参考 Google Play版 Telegram ,包名: org.telegram.messenger
+          // snapshotUrls: [
+          //   'https://i.gkd.li/i/27050091', // 这张照片
+          //   'https://i.gkd.li/i/27050227', // 這張照片
+          //   'https://i.gkd.li/i/26645509', // This photo
+          //   'https://i.gkd.li/i/27049721', // 该媒体
+          // ],
+          exampleUrls: 'https://e.gkd.li/322731e7-f595-4775-9986-a9cff001e861',
+          activityIds: 'org.telegram.ui.LaunchActivity',
+        },
+      ],
+    },
   ],
 });
