917900321c8cde2a61392d2c31d0608482d38fb3
Lin-arm <85251920+Lin-arm@users.noreply.github.com>
2026-06-10 15:06:39 +0800

    perf: 番茄免费小说 分段广告-阅读页面广告
    - 将 局部广告-阅读页面广告 合并到 分段广告-阅读页面广告
    - 删掉部分相似快照
    - 其它....

--- src/apps/com.dragon.read.ts
+++ src/apps/com.dragon.read.ts
@@ -23,41 +23,6 @@ export default defineGkdApp({
       ],
     },
     {
-      key: 0,
-      name: '局部广告-阅读页面广告',
-      fastQuery: true,
-      activityIds: '.reader.ui.ReaderActivity',
-      rules: [
-        {
-          key: 1,
-          name: '点击[x]掉',
-          matches:
-            '@[name$="ImageView" || getChild(0).name$="ImageView"][clickable=true][width<102 && height<102] <n [childCount>1] >(2,3) [text*="查看" || text$="优惠" || text^="立即" || text^="马上" || text*="参与" || text*="免费" || (text*="领" && text*="券") || (text*="返" && text*="金币")][text.length<10]',
-          snapshotUrls: [
-            'https://i.gkd.li/i/12908734', // 查看详情
-            'https://i.gkd.li/i/18138903', // 立享优惠
-            'https://i.gkd.li/i/21623147', // 立即前往
-            'https://i.gkd.li/i/14622531', // 立即玩
-            'https://i.gkd.li/i/25174203', // 马上参与
-            'https://i.gkd.li/i/26347335', // 参与讨论
-            'https://i.gkd.li/i/13520314', // 领60元券
-            'https://i.gkd.li/i/24706223', // 免费观看
-            'https://i.gkd.li/i/14548657', // 返1098金币
-            'https://i.gkd.li/i/14810480', // 返780金币
-          ],
-          exampleUrls: 'https://e.gkd.li/3de0d5d9-0c02-4fe7-b5e8-b9fdb6688f8e',
-        },
-        {
-          key: 3,
-          name: '点击[关闭此条广告]',
-          matches: '@[clickable=true] > [text="关闭此条广告"]',
-          exampleUrls:
-            'https://m.gkd.li/57941037/cf9d0574-dc89-4f03-ba01-eb9bcc97925f',
-          snapshotUrls: 'https://i.gkd.li/i/14540281', // 分段广告-第二段?
-        },
-      ],
-    },
-    {
       key: 1,
       name: '更新提示',
       fastQuery: true,
@@ -205,15 +170,53 @@ export default defineGkdApp({
       activityIds: '.reader.ui.ReaderActivity',
       rules: [
         {
-          /*
-           ** 目前新广告中该规则基本已无法匹配，优化匹配规则
-           ** 同时现阶段该规则与“局部广告-阅读页面广告”中的“key=5"规则相冲突，已在其中做删除
-           ** actionCd: 因同类型广告出现“当前可点击”与“3秒后可点击”两种状态，而在“3秒后可点击”状态下
-           **           按钮未被隐藏会被规则持续匹配，因此选择此属性降低“3秒后可点击”状态下的点击频率
-           */
           key: 0,
-          name: '点击[反馈]按钮',
-          actionCd: 3500,
+          matches:
+            '@[clickable=true][width<102 && height<102] <n [childCount>1] >(2,3) [text*="查看" || text$="优惠" || text^="立即" || text^="马上" || text*="参与" || text*="免费" || (text*="领" && text*="券") || (text*="返" && text*="金币")][text.length<10]',
+          snapshotUrls: [
+            'https://i.gkd.li/i/12908734', // 查看详情
+            'https://i.gkd.li/i/18138903', // 立享优惠
+            'https://i.gkd.li/i/21623147', // 立即前往
+            'https://i.gkd.li/i/14622531', // 立即玩
+            'https://i.gkd.li/i/25174203', // 马上参与
+            'https://i.gkd.li/i/26347335', // 参与讨论
+            'https://i.gkd.li/i/13520314', // 领60元券
+            'https://i.gkd.li/i/24706223', // 免费观看
+            'https://i.gkd.li/i/14548657', // 返1098金币
+            'https://i.gkd.li/i/14810480', // 返780金币
+          ],
+          exampleUrls: 'https://e.gkd.li/3de0d5d9-0c02-4fe7-b5e8-b9fdb6688f8e',
+        },
+        {
+          key: 1,
+          matches:
+            '@ImageView[clickable=true][childCount=0][visibleToUser=true] < FrameLayout - LinearLayout >2 [text="广告"]',
+          exampleUrls: 'https://e.gkd.li/c172db67-a489-488b-a5f5-35aa9657c444',
+          snapshotUrls: 'https://i.gkd.li/i/18724040',
+        },
+        {
+          key: 2,
+          matches:
+            '@View[clickable=true][width<121 && height<121] < View < View < ComposeView < * < * < [childCount=1] + FrameLayout >2 [childCount=3][getChild(0).text*="/"] >2 [text*=":"][text.length=5]',
+          snapshotUrls: [
+            'https://i.gkd.li/i/28781063',
+            'https://i.gkd.li/i/28781061',
+          ],
+        },
+        {
+          key: 3,
+          matches:
+            '@[clickable=true][width<121 && height<121] <<n FrameLayout[visibleToUser=true][childCount=1] -n [childCount=3] >6 [getChild(0).text*="/"] >2 [text*=":"][text.length=5]',
+          exampleUrls: 'https://e.gkd.li/ab2021a9-8e5c-4d2a-8df1-8c6aff4e38f6',
+          snapshotUrls: [
+            'https://i.gkd.li/i/24189900',
+            'https://i.gkd.li/i/24205810',
+          ],
+        },
+        {
+          key: 4,
+          name: '点击[反馈]',
+          actionCd: 3500, // 同类型广告有“当前可点击”与“3秒后可点击”两种状态
           actionDelay: 200,
           matches:
             '[text="反馈" || desc="反馈"][visibleToUser=true][name$="UIText" || name$="ViewGroup"]',
@@ -226,43 +229,24 @@ export default defineGkdApp({
             'https://i.gkd.li/i/24128141',
           ],
         },
+
+        // 第二段
         {
-          key: 1,
-          matches:
-            '@ImageView[clickable=true][childCount=0][visibleToUser=true] < FrameLayout - LinearLayout >2 [text="广告"]',
-          exampleUrls: 'https://e.gkd.li/c172db67-a489-488b-a5f5-35aa9657c444',
-          snapshotUrls: 'https://i.gkd.li/i/18724040',
-        },
-        //key: 2广告多发于夜间模式
-        {
-          key: 2,
-          name: '底部横幅广告-点击[x]',
-          matches:
-            'ViewGroup[childCount>4] > @ViewGroup[childCount=1][clickable=true][focusable=true][visibleToUser=true][width<50 && height<50] +n [desc!=null]',
-          exampleUrls: 'https://e.gkd.li/ab2021a9-8e5c-4d2a-8df1-8c6aff4e38f6',
-          snapshotUrls: [
-            'https://i.gkd.li/i/24189866',
-            'https://i.gkd.li/i/24189900',
-            'https://i.gkd.li/i/24189911',
-            'https://i.gkd.li/i/24189915',
-            'https://i.gkd.li/i/24205796',
-            'https://i.gkd.li/i/24205810',
+          key: 50,
+          preKeys: [0, 1, 2, 3, 4],
+          name: '②点击[不感兴趣/关闭此广告]',
+          anyMatches: [
+            '@[clickable=true] > [text="不感兴趣" || text^="关闭此"]',
+            '[text="不感兴趣" || text^="关闭此"][clickable=true]',
           ],
-        },
-        {
-          preKeys: [0, 1, 2],
-          name: '点击[不感兴趣]/[关闭此广告]',
-          matches: '[text="不感兴趣" || text="关闭此广告"]',
+          snapshotUrls: [
+            'https://i.gkd.li/i/13816454', //不感兴趣
+            'https://i.gkd.li/i/14913207', //不感兴趣
+            'https://i.gkd.li/i/24128392', //不感兴趣
+            'https://i.gkd.li/i/24189605', //关闭此广告
+            'https://i.gkd.li/i/14540281', //关闭此条广告
+          ],
           exampleUrls: 'https://e.gkd.li/e04bcb90-ad61-43d9-97e9-b4f6e3873320',
-          snapshotUrls: [
-            'https://i.gkd.li/i/13520219',
-            'https://i.gkd.li/i/13674550',
-            'https://i.gkd.li/i/13816454',
-            'https://i.gkd.li/i/14913207',
-            'https://i.gkd.li/i/18724041',
-            'https://i.gkd.li/i/24128392',
-            'https://i.gkd.li/i/24189605',
-          ],
         },
       ],
     },
