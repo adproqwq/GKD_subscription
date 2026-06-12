85e6d39523e41bc76787304b2b77f5317e0b4eef
Lin-arm <85251920+Lin-arm@users.noreply.github.com>
2026-06-12 19:22:38 +0800

    feat: Reddit 分段广告-卡片广告
    - 快照来源: tg群

--- src/apps/com.reddit.frontpage.ts
+++ src/apps/com.reddit.frontpage.ts
@@ -50,5 +50,43 @@ export default defineGkdApp({
         },
       ],
     },
+    {
+      key: 6,
+      name: '分段广告-卡片广告',
+      desc: '①点击卡片右上角 ②点击[隐藏]',
+      activityIds: 'com.reddit.launch.main.MainActivity',
+      rules: [
+        {
+          key: 1,
+          name: '①点击卡片右上角',
+          matches:
+            '@[id="post_overflow"] <4 [id="post_header"] <2 [id="promoted_post_unit"]',
+          snapshotUrls: 'https://i.gkd.li/i/28876494',
+        },
+        {
+          key: 2,
+          name: '①坐标点击卡片右上角',
+          position: {
+            left: 'width * 0.9106',
+            top: 'width * 0.0657',
+          },
+          matches:
+            'View[top=prev.bottom] - @View[height<200][top=prev.top] <2 [id="conversation_screen_ad"]',
+          snapshotUrls: 'https://i.gkd.li/i/28876944',
+        },
+
+        // 第二段
+        {
+          key: 20,
+          preKeys: [1, 2],
+          name: '②点击[隐藏]',
+          matches: '@[clickable=true] > Button - [text="Hide"]',
+          snapshotUrls: [
+            'https://i.gkd.li/i/28876631',
+            'https://i.gkd.li/i/28876636',
+          ],
+        },
+      ],
+    },
   ],
 });
