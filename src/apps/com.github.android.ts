aad76eded4595d5c4c20db16beb07ce1acfe0db1
Ling0402 <cjy081219@Gmail.com>
2026-06-13 11:17:00 +0800

    feat: Github 功能类-自动批准授权验证
    - 功能类-自动关闭登录批准结果 -> 功能类-自动批准授权验证

--- src/apps/com.github.android.ts
+++ src/apps/com.github.android.ts
@@ -6,16 +6,27 @@ export default defineGkdApp({
   groups: [
     {
       key: 1,
-      name: '功能类-自动关闭登录批准结果',
-      desc: '登录批准/不批准-关闭',
+      name: '功能类-自动批准授权验证',
+      desc: '输入2位数字后-> 批准-> 关闭',
+      activityIds: '.twofactor.TwoFactorActivity',
       rules: [
         {
-          activityIds: '.twofactor.TwoFactorActivity',
+          key: 0,
+          matches: '[text*="@"] + EditText[text.length=2][visibleToUser=true]',
+          snapshotUrls: 'https://i.gkd.li/i/28906388',
+          exampleUrls: 'https://e.gkd.li/86a9d677-6f59-4a3a-81d0-8bb0d6847ca9',
+        },
+        {
+          preKeys: [0],
           matches:
             '[text="无法批准" || text="登录已批准"] +n @[clickable=true] > [text="关闭"][visibleToUser=true]',
           snapshotUrls: [
-            'https://i.gkd.li/i/25366549',
-            'https://i.gkd.li/i/25366553',
+            'https://i.gkd.li/i/25366553', // 已批准
+            'https://i.gkd.li/i/25366549', // 未批准
+          ],
+          exampleUrls: [
+            'https://e.gkd.li/e18eed95-993a-4148-b486-27e64e709188',
+            'https://e.gkd.li/c165be1d-f0d4-47d0-8a39-3aac562885b1',
           ],
         },
       ],
