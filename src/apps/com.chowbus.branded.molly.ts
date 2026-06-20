74ec66ff8bcc52a21ffe8b0ffa5ae3a26e275110
Ling0402 <cjy081219@Gmail.com>
2026-06-20 08:24:50 +0800

    feat: Molly Tea 开屏广告 (#207)
    close: #207
    - `Skip`节点本身无快查,所以开屏广告无法匹配
    - 本app还有**其他形式**跳过,故不加`开屏广告全局禁用`

---
+++ src/apps/com.chowbus.branded.molly.ts
@@ -0,0 +1,27 @@
+import { defineGkdApp } from '@gkd-kit/define';
+
+export default defineGkdApp({
+  id: 'com.chowbus.branded.molly',
+  name: 'Molly Tea',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      matchTime: 10000,
+      forcedTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      matchRoot: true,
+      rules: [
+        {
+          fastQuery: true,
+          activityIds: 'com.chowbus.branded.branded_app.MainActivity',
+          matches:
+            '@[desc^="Skip"][visibleToUser=true] < [childCount=2] <<5 [id="android:id/content"]',
+          snapshotUrls: 'https://i.gkd.li/i/29147518',
+          exampleUrls: 'https://e.gkd.li/a43170a0-8196-49d8-bb4d-3554aeae1900',
+        },
+      ],
+    },
+  ],
+});
